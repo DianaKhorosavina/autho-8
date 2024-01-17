908eb7106ac5ce8a12686b4755840dd4fab5d9f4
Diana Khorosavina <dkhorosavina@gmail.com>
2024-01-17 13:42:11 +0300

    initial commit

---
+++ src/test/java/ru/netology/test/BankLoginTest.java
@@ -0,0 +1,57 @@
+package ru.netology.test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+import java.sql.SQLException;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanAuthCode;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+public class BankLoginTest {
+
+    LoginPage loginPage;
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        cleanAuthCode();
+    }
+
+    @BeforeAll
+    static void tearDownAll() throws SQLException {
+        cleanDatabase();
+    }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    void ShouldSuccessfullyLogin() throws SQLException {
+        var authInfo = DataHelper.getAughtInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    void ShouldSetErrorNotificationMassageWithRandomUser() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! \nНеверно указан логин или пароль");
+    }
+
+    @Test
+    void ShouldSetErrorNotificationMassageWithRandomVerificationCode() {
+        var authInfo = DataHelper.getAughtInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка!\nНеверно указан код!Попробуйте еще раз.");
+    }
+}
